35ac402e8acf455d7af7643dc5999868788205da
Linda Persson <lindapersson-@live.se>
2024-01-29 09:08:57 +0000

    <feat>added choose game rules function

--- run.py
+++ run.py
@@ -6,16 +6,37 @@ def game_intro ():
     print('Do you have what it takes to make it all the way to the Premier League?')
     print('If you make the right decisions, anything is possible!')
     print('Create your player and let the game begin!')
-    game_rules()
+    choose_game_rules()
+
+def choose_game_rules():
+    """
+    Explainns the rules/ how to play the game
+    """
+    chose_game_rules = input(
+        'But first, do you want to read the rules/ how to play the game? y/n \n'
+    )
+    if chose_game_rules == 'y':
+        game_rules()
+    elif chose_game_rules == 'n':
+        start_game()
+    else:
+        print('wrong character, please try again')
+        choose_game_rules()
+
+
+
 
 def game_rules():
-    print('game rules')
+    print('start rules')
 
 
-
-
+def start_game():
+    print('start game')
 
 def main():
+    """
+    starts the first function
+    """
     game_intro()
 
 
