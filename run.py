6331d55a13722532fedc42ab7cd646241e2bb85c
Linda Persson <lindapersson-@live.se>
2024-01-29 11:12:31 +0000

    <feat>made the player name acceible in all fuctions

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def choose_game_rules():
     if chose_game_rules == 'y':
         game_rules()
     elif chose_game_rules == 'n':
-        start_game()
+        choose_name()
     else:
         print('wrong character, please try again')
         choose_game_rules()
@@ -26,49 +26,71 @@ def game_rules():
     """
     The rules of the game/how to play
     """
-    print('Write the rules XXXX and are you reday to start the game? y/n \n')
-    if chose_game_rules == 'y':
-        start_game()
-    elif chose_game_rules == 'n':
+    print('Write the rules XXXX and are you reday to start the game? \n')
+    reday_to_start = input('y/n \n')
+    if reday_to_start == 'y':
+        choose_name()
+    elif reday_to_start == 'n':
         print('To bad, maybe next time')
         game_intro ()
     else:
         print('wrong character, please try again')
+        game_rules()
 
-def start_game():
-    """
-    Starts the game
-    """
+def choose_name():
     print("Let´s begin!")
     player_name = input(
         'What´s your name fotballplayer? \n'
     )
+    start_game(player_name)
+    return player_name
+
+def start_game(player_name):
+    """
+    Starts the game
+    """
     print(f"Hello {player_name}, let´s see how long you can go!\n")
     print(f"{player_name} is a young and promising football player who loves and lives for the sport.")
     print(f"Currently, {player_name} is part of a smaller team in Division 1 in Sweden, GIF Sundsvall.")
     print(f"But {player_name} trains every day to progress further and what {player_name} enjoys training the most is (choose 1 or 2):")
+    choose_training(player_name)
+
+def choose_training(player_name): 
     choose_training = input (
         '1. Strenght \n2. Endurance \n'
         )
     if choose_training == '1':
-        training_strenght()
+        training_strenght(player_name)
     elif choose_training == '2':
-        training_endurance()
+        training_endurance(player_name)
     else:
         print('wrong character, please try again')
-        choose_training()
+    choose_training(player_name)
+    
         
-def training_strenght():
-    print('styrka funktionen')
-def training_endurance():
-    print('kondiion functioen')
+def training_strenght(player_name):
+    """
+    Runs when the users choose strenght in start_game
+    """
+    print('styrka')
 
+def training_endurance(player_name):
+    """
+    Runs when the users choose endurance in start_game
+    """
+    print('kondiion functioen')
 
 def main():
     """
     starts the first function
     """
     game_intro()
+    player_name = choose_name()
+    start_game(player_name)
+    choose_training(player_name)
+    training_strenght(player_name)
+    training_endurance(player_name)
+
 
 
 main()
