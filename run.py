47dd3e7e8c49a6d562c3c2776a6f84c11a1e50aa
Linda Persson <lindapersson-@live.se>
2024-01-29 08:46:04 +0000

    <feat>Defined the game intro function

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 def game_intro ():
-    print('Welcomme!')
+    """
+    Run games intro
+    """
+    print('Welcomme Welcome to Fantasy Football!') 
+    print('Do you have what it takes to make it all the way to the Premier League?')
+    print('If you make the right decisions, anything is possible!')
+    print('Create your player and let the game begin!')
+    game_rules()
 
-game_intro()
+def game_rules():
+    print('game rules')
+
+
+
+
+
+def main():
+    game_intro()
+
+
+main()
