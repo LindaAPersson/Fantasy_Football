5b237959a27603648100b69223744475866248d8
Linda Persson <lindapersson-@live.se>
2024-01-30 19:19:37 +0000

    added story document to put the text in

---
+++ story.py
@@ -0,0 +1,5 @@
+def start_game_story():
+    print(f"Hello {player_name}, let´s see how long you can go!\n")
+    print(f"{player_name} is a young and promising football player who loves and lives for the sport.")
+    print(f"Currently, {player_name} is part of a smaller team in Division 1 in Sweden, GIF Sundsvall.")
+    print(f"But {player_name} trains every day to progress further and what {player_name} enjoys training the most is")
