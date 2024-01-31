d303a2af9317b19b4a35ac89ee2fe7d858f2fb83
Linda Persson <lindapersson-@live.se>
2024-01-31 15:03:54 +0000

    <docs>added effect py to make a slow print fuction in

---
+++ effect.py
@@ -0,0 +1,11 @@
+import sys
+import time
+
+def slow(all_strings):
+    """
+    Runs all story strings in story.py on slow
+    """
+    for all_letters in all_strings + "\n":
+        sys.stdout.write(all_letters)
+        sys.stdout.flush()
+        time.sleep(1./10)
